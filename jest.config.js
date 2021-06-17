2dd6df53000f39d513951066291a532c8216ba65
Marcelo Fontana <marcelo.fontana@fluig.com>
2021-06-17 20:58:04 -0300

    build: update project configuration files
    Add commitlint
    
    Add stylelint
    
    Update jest config to add coverage and create some tests with snapshots
    
    Update gitignore to ignore the coverage folder

--- jest.config.js
+++ jest.config.js
@@ -1,3 +1,14 @@
 module.exports = {
   preset: "@vue/cli-plugin-unit-jest/presets/typescript-and-babel",
+  testMatch: ["**/+(*.)+(spec).+(ts)?(x)"],
+  collectCoverage: true,
+  collectCoverageFrom: ["<rootDir>/src/**/**/*.vue", "!**/node_modules/**"],
+  coverageThreshold: {
+    global: {
+      statements: 85,
+      branches: 85,
+      lines: 85,
+      functions: 85,
+    },
+  },
 };
