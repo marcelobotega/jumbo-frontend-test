c8528dc48895dadb5c4f55073a14dc2c08a9d44d
Marcelo Fontana <marcelo.fontana@fluig.com>
2021-06-17 21:20:03 -0300

    feat: add tailwindcss

--- src/App.vue
+++ src/App.vue
@@ -9,14 +9,6 @@
 </template>
 
 <style lang="scss">
-#app {
-  color: #2c3e50;
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-}
-
 #nav {
   padding: 30px;
 
